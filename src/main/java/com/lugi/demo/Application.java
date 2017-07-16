5913344ee1a1ed7dd17326442603eac837cf7ce4
lucare <fengchangsheng123@qq.com>
2017-07-16 22:51:47 +0800

    update

--- src/main/java/com/lugi/demo/Application.java
+++ src/main/java/com/lugi/demo/Application.java
@@ -1,12 +1,17 @@
 package com.lugi.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * Created by fengcs on 2017/7/14.
  */
 @SpringBootApplication
+@MapperScan("com.lugi.demo.dao")
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
